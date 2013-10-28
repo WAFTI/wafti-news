6fecef9b9f16cbff74f3b75f30d0cc4542b7fecb
Ross Fenning <ross.fenning@gmail.com>
2013-10-28 22:18:27 +0000

    First stab at making this runnable on Heroku.

--- wafti_news/settings/common.py
+++ wafti_news/settings/common.py
@@ -26,9 +26,11 @@ DATABASES = {
 
 DATABASES = {'default': dj_database_url.config(default='sqlite:////tmp/wafti-news.db')}
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -66,7 +68,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = 'staticfiles'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
